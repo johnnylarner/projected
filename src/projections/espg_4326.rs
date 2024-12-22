f7e434f27adcf15961a245ccae972a7ae522770f
johnnylarner <james.richardson.2556@gmail.com>
2024-12-22 23:36:28 +0100

    refactor: make crs string a module const

--- src/projections/espg_4326.rs
+++ src/projections/espg_4326.rs
@@ -1,9 +1,9 @@
+pub const ESPG_4326: &str = "ESPG:4326";
+
 #[derive(Clone, Debug)]
 pub struct Espg4326;
 
 pub trait ToEspg4326 {
-    const ESPG_4326: &str = "ESPG:4326";
-
     type Output;
 
     fn to_espg_4326(&self) -> Self::Output;
