a41f2e6106dc84e8003f95c87ae0c6cf58df5b72
johnnylarner <james.richardson.2556@gmail.com>
2025-01-13 07:53:55 +0100

    refactor: make 4326 default for new

--- src/geometries/point.rs
+++ src/geometries/point.rs
@@ -6,24 +6,18 @@ use proj::Proj;
 use crate::projections::{Epsg3035, Epsg4326, ToEpsg3035, ToEpsg4326, EPSG_3035, EPSG_4326};
 
 #[derive(Clone, Debug)]
-pub struct ProjectedPoint<Projection> {
+pub struct ProjectedPoint<Projection = Epsg4326> {
     point: Point,
     _marker: PhantomData<Projection>,
 }
 
-macro_rules! point_constructor {
-    ( for $( $t:ty ),* ) => {
-        $(
-        impl ProjectedPoint<$t> {
-            pub fn new(point: Point) -> ProjectedPoint<$t> {
-                Self {
-                    point,
-                    _marker: PhantomData,
-                }
-            }
+impl ProjectedPoint {
+    pub fn new(point: geo::Point) -> ProjectedPoint<Epsg4326> {
+        Self {
+            point,
+            _marker: PhantomData,
         }
-        )*
-    };
+    }
 }
 
 macro_rules! point_value {
@@ -38,7 +32,6 @@ macro_rules! point_value {
     };
 }
 
-point_constructor!(for Epsg4326, Epsg3035);
 point_value!(for Epsg4326, Epsg3035);
 
 impl ToEpsg4326 for ProjectedPoint<Epsg3035> {
@@ -83,7 +76,7 @@ mod projections {
     #[test]
     fn change_when_converted() {
         let p = Point::new(2.0, 2.1);
-        let p = ProjectedPoint::<Epsg4326>::new(p);
+        let p = ProjectedPoint::new(p);
         let projected = p.to_epsg_3035();
 
         assert_ne!(p.point(), projected.point());
